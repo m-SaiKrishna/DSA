e4a8312c9861b3b632bf38d7e3aedc21fec08a20
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-06-17 14:50:10 +0530

    first program

---
+++ Problem1/HelloWorld.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class HelloWorld{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str);
+    }
+}
