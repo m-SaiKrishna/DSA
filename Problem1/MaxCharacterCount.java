70e697a16ab1b4102a4d9028f7353b4f71320362
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-06-21 17:43:32 +0530

    Maximum Occurence Character

---
+++ Problem1/MaxCharacterCount.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class MaxCharacterCount{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int[] arr = new int[26];
+        int max = maxFrequencyCharacter(arr, str);
+        System.out.println("Max Frequency "+max);
+    }
+    
+    public static int maxFrequencyCharacter(int[]arr,String str){
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            int temp = ch - 'a';
+            arr[temp]++;
+    
+        }
+        int max = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] > max) max = arr[i];
+        }
+        return max;
+        
+    }
+}
