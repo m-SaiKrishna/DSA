2cb46450c32d64f7ef1d85e6892c7d6d19433307
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-06-28 22:08:46 +0530

    Add Fractions

---
+++ Problem1/AddFractions.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class AddFractions{
+public static void main(String args[]){
+   String[] arr = {"2/6+2/6", "13/10+17/10"};
+
+for(String st : arr) {
+    String[] ans = st.split("\\+"); // split each string by '+'
+    
+    for(int i = 0; i < ans.length; i++) {
+        System.out.println(ans[i]); // print each fraction
+    }
+}
+}
+
+public static void addFractions(String[] arr){
+    List<String> list = new ArrayList<>();
+    for(int i = 0; i < arr.length; i++){
+        String in = arr[i];
+        String st = "";
+        for(int j = 0; j < in.length(); j++){
+            if(in.charAt(j) == '+') {
+                list.add(st);
+                st = "";
+
+            }
+            else{
+                st += in.charAt(j);
+            }
+        }
+        list.add(st);
+    }
+
+    for(String ele : list) System.out.println(ele);
+}
+}
