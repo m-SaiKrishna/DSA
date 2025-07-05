ee6ce1513ad70099ae7d8ae38dd72d2fc43f4fa1
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-07-05 21:49:06 +0530

    number of good paris

--- HashMap/NumberOfGoodPairs.java
+++ HashMap/NumberOfGoodPairs.java
@@ -21,6 +21,17 @@ class NumberOfGoodPairs{
         }
         return pairs;
     }
+
+
+    public static int numberOf(int[] arr){
+        int pairs = 0;
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i+1; j<arr.length; j++){
+                if(arr[i] == arr[j]) pairs++;
+            }
+        }
+        return pairs;
+    }
 }
 
 
