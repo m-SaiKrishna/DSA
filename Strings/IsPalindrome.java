d426de5ec2a8dd55fbfc5cee1e6d9a106b59733c
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-07-08 21:30:15 +0530

    updated

--- Strings/IsPalindrome.java
+++ Strings/IsPalindrome.java
@@ -8,17 +8,17 @@ public class IsPalindrome {
 }
 
 
-// public static boolean isPalindrome(String str) {
-//         String string1 = str.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
-//         int i = 0 , j = string1.length() - 1;
-//         while(i < j){
-//             if(string1.charAt(i) != string1.charAt(j)) {
-//                 return false;
-//             }
-//             i++;
-//             j--;
-//         } 
-//            return true;
-//     }
+public static boolean isPalindrome(String str) {
+        String string1 = str.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        int i = 0 , j = string1.length() - 1;
+        while(i < j){
+            if(string1.charAt(i) != string1.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        } 
+           return true;
+    }
 }
 
