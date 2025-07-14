75604efdd8d4a6e0df63eda55319c7045acb0c7c
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-07-14 12:50:57 +0530

    Test Class

---
+++ Problems/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        int n = 8, p = 3;
+        System.out.println(n >> p);
+        StringBuilder st = new  StringBuilder();
+        
+    }
+}
