6895eaa0c3d5884701ce0586c724b921a5ac9fa8
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-07-14 18:03:39 +0530

    Find Minimum in Rotated Sorted Array

---
+++ LeetCode/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class FindMinimumInRotatedSortedArray {
+    public static int findMin(int[] nums) {
+        int low = 0, high = nums.length-1,ans = Integer.MAX_VALUE;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[low] <= nums[mid]){
+                ans = Math.min(ans,nums[low]);
+                low = mid + 1;
+            }
+            else{
+                ans = Math.min(ans,nums[mid]);
+                high = mid - 1;
+            }
+
+            
+    }
+        return ans;
+}
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0; i<arr.length; i++) arr[i] = sc.nextInt();
+    int minimumElement = findMin(arr);
+    System.out.println(minimumElement);
+}
+}
