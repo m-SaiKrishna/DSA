dd31d469103d78555af190f6fdfdad135b7db28f
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-07-15 23:03:35 +0530

    Recursion Pratice

---
+++ Recursion/Text.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Text{
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //print(n);
+        //print1ToN(n,1);
+        //printNTo1(n);
+     }   
+
+    //  public static void print(int n){
+    //     if(n <= 0) 
+    //        return;
+    //     n--;
+    //     System.out.println("hello");
+    //     print(n);
+    // }
+
+
+
+
+    //  public static void print1ToN(int n,int num){
+    //     if(num > n) return;
+    //     System.out.println(num);
+    //     print1ToN(n,num+1);
+    //  }
+
+
+    // public static void printNTo1(int n){
+    //     if(n <= 0) return;
+    //     printNTo1(n);
+    //     System.out.println(n);
+    //     n--;
+    // }
+
+
+    
+    
+
+}
+
