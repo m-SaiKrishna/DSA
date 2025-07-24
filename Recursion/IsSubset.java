cd778b39630328abf185812ebf0c04c70204c430
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-07-24 23:48:58 +0530

    is subset

--- Recursion/IsSubset.java
+++ Recursion/IsSubset.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class IsSubset {
     public static void main(String[] args) {
         int[] arr = {1, 3, 1, 10, 4};
+        int cnt = 0;
         int targetSum = 8;
         int n = arr.length;
         List<Integer> list = issubset(0, arr, n, 0, targetSum, new ArrayList<>());
