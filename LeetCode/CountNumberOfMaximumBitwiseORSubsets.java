547ecc2b77ae0323b4d7b2265465ca2d1bf27b76
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-07-28 18:39:54 +0530

    code updated

---
+++ LeetCode/CountNumberOfMaximumBitwiseORSubsets.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class CountNumberOfMaximumBitwiseORSubsets {
+    public static void main(String[] args) {
+        int[] dp =  new int[1 << 17];
+        System.out.println(Arrays.toString(dp));
+    }
+}
+
+
+
+
+
--- LeetCode/LongestIncreasingSubSequence.java
+++ LeetCode/LongestIncreasingSubSequence.java
@@ -4,6 +4,7 @@ public class LongestIncreasingSubSequence {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
+        String st = "";
         for(int i=0; i<arr.length; i++) arr[i] = sc.nextInt();
         System.out.println(lengthOfLIS(arr));
     }
