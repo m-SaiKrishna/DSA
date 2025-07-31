2065963e5081f200180d5d5ec2877a5faa4fb6f3
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-07-31 18:35:26 +0530

    Insertion Sort

---
+++ Problems/Main.java
@@ -0,0 +1,25 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+       int[] arr = {12,11,13,5,6};
+       insertionSort(arr);
+       
+    }
+    
+    public static void insertionSort(int[] arr){
+        
+        for(int i = 1; i < arr.length; i++){
+            
+        
+    }
+    }
+    
+    public static void swap(int[] arr,int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+}
