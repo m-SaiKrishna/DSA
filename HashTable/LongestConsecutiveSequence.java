c007675278c101aa56b21d36b030fe3aa19d3c97
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-08-04 10:35:00 +0530

    Longest Consecutive Sequence

---
+++ HashTable/LongestConsecutiveSequence.java
@@ -0,0 +1,32 @@
+package HashTable;
+
+import java.util.Arrays;
+
+public class LongestConsecutiveSequence {
+    public static void main(String[] args) {
+        int[] arr = {0};
+        System.out.println(longestConsecutiveSequence(arr));
+    }
+
+
+    public static int  longestConsecutiveSequence(int[] arr){
+        int[] hash = new int[10+1];
+        int ans = 1, max = 0;
+        for(int i = 0; i < arr.length; i++){
+            hash[arr[i]]++;
+        }
+        for(int i = 0; i < arr.length; i++){
+            if(hash[i] != 0){
+                ans++;
+            }
+            else{
+                max = Math.max(max,ans);
+                ans = 0;
+            }
+        }
+        return max;
+    }
+}
+
+
+
