d2e1bfa8b6f5ebeb3ea62b878fc5a1de841e017f
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-08-11 18:41:21 +0530

    Max Window Sum

---
+++ TwoPointers/MaxWindowSum.java
@@ -0,0 +1,23 @@
+package TwoPointers;
+import java.util.*;
+public class MaxWindowSum{
+    public static void main(String[] args) {
+        int[] arr = {1,5,4,2,9,9,9};
+        int k = 3;
+        System.out.println(maxWindowSum(arr, k));
+    }
+
+    public static int maxWindowSum(int[] arr,int k){
+        int max = 0;
+        for(int i = 0; i <= arr.length - k; i++){
+            int sum = 0, j = i;
+            while(j < i + k){
+                sum += arr[j];
+                j++;
+                
+            }
+            max = Math.max(max,sum);
+        }
+        return max;
+    }
+}
