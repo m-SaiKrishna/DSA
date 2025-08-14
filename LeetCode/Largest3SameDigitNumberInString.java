29fbc24fcf21fb5c1c5f4c35bf57c939f0d6b82d
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-08-14 11:07:52 +0530

    largest digit same

---
+++ LeetCode/Largest3SameDigitNumberInString.java
@@ -0,0 +1,14 @@
+public class Largest3SameDigitNumberInString {
+    public String largestGoodInteger(String num) {
+        String result = "";
+        for (int i = 0; i < num.length() - 2; i++) {
+            if (num.charAt(i) == num.charAt(i + 1) && num.charAt(i) == num.charAt(i + 2)) {
+                String candidate = num.substring(i, i + 3);
+                if (candidate.compareTo(result) > 0) {
+                    result = candidate;
+                }
+            }
+        }
+        return result;
+    }
+}
