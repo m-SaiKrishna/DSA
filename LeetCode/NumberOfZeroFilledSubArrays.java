10c946eeef75dfa52d6b0d3b7c8295e95941a83b
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-08-19 16:03:33 +0530

    Number Of Zero Filled SubArrays

---
+++ LeetCode/NumberOfZeroFilledSubArrays.java
@@ -0,0 +1,19 @@
+public class NumberOfZeroFilledSubArrays {
+    public static void main(String[] args) {
+        int[] arr = {0,0,0,2,0,0};
+        System.out.println(zeroFilledSubarray(arr));
+    }
+     public static long zeroFilledSubarray(int[] nums) {
+      long cnt = 0, max = 0;
+      for(int num : nums){
+        if(num == 0){
+            max += 1;
+            cnt += max;
+        }
+        else{
+            max = 0;
+        }
+      }
+       return cnt;
+    }
+}
