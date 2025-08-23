72916d32181138bb271fbfc51963cd1a3b2d1e67
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-08-23 22:58:03 +0530

    code updated

--- LeetCode/FindMinimumInRotatedSortedArray.java
+++ LeetCode/FindMinimumInRotatedSortedArray.java
@@ -24,6 +24,7 @@ public static void main(String[] args) {
     int n = sc.nextInt();
     int[] arr = new int[n];
     for(int i = 0; i<arr.length; i++) arr[i] = sc.nextInt();
+    // for(int i = 0; i<arr.length; i++) arr[i] = sc.nextInt();
     int minimumElement = findMin(arr);
     System.out.println(minimumElement);
 }
