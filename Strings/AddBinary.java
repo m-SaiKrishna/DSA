8e5bc51069ddb4b1bb76120401bbc557a81859b7
m-SaiKrishna <saikrishna5112004@gmail.com>
2025-09-15 22:36:16 +0530

    code updated

--- Strings/AddBinary.java
+++ Strings/AddBinary.java
@@ -16,6 +16,7 @@ class AddBinary{
         while(i >= 0 || j >= 0 || carry > 0){
             int sum = carry;
             if(i >= 0) sum += str1.charAt(i) - '0';
+            // if(i >= 0) sum += str1.charAt(i) - '0';
             if(j >= 0) sum += str2.charAt(j) - '0';
             st.append(sum % 2);
             carry = sum / 2;
